569937e1acbd6055b3c20faeedf16dfadce1205c
Shikher Verma <root@shikherverma.com>
2016-03-20 15:38:44 +0530

    Add pkgs

---
+++ hardware_camera/include/hardware_camera/resized_subscriber.h
@@ -0,0 +1,17 @@
+#include <image_transport/simple_subscriber_plugin.h>
+#include <hardware_camera/ResizedImage.h>
+
+class ResizedSubscriber : public image_transport::SimpleSubscriberPlugin<hardware_camera::ResizedImage>
+{
+public:
+  virtual ~ResizedSubscriber() {}
+
+  virtual std::string getTransportName() const
+  {
+    return "resized";
+  }
+
+protected:
+  virtual void internalCallback(const typename hardware_camera::ResizedImage::ConstPtr& message,
+                                const Callback& user_cb);
+};
