29146e955cda9340b4be0980527abf6f379416ae
Shikher Verma <root@shikherverma.com>
2016-03-22 23:13:41 +0530

    modified image node to publish distances

--- src/image.cpp
+++ src/image.cpp
@@ -6,41 +6,30 @@
 #include "std_msgs/Int8.h"
 #include <fstream>
 #include "std_msgs/Float32MultiArray.h"
-
+#include "std_msgs/Float64.h"
+#include <ctime>
+#include <iostream>
 #include <sstream>
 using namespace std;
 using namespace cv;
 
 int main(int argc, char* argv[])
 {
-      
-
     int height,width,step,channels;  //parameters of the image we are working on
     int i,j,k,t1min=29,t1max=128,t2min=72,t2max=153,t3min=133,t3max=239,Rmin=0,Rmax=0; // other variables used
-    VideoWriter output_cap(argv[2],CV_FOURCC('D','I','V','X'),25,Size(640,480));
+
+    VideoWriter output_cap("buoy.avi",CV_FOURCC('D','I','V','X'),25,Size(640,480));
 
     ros::init(argc, argv, "image");
     ros::NodeHandle n;
 
-    ros::Publisher pub = n.advertise<std_msgs::Float32MultiArray>("balls", 1000);
+    ros::Publisher puby = n.advertise<std_msgs::Float64>("yDistance", 1000);
+    ros::Publisher pubx = n.advertise<std_msgs::Float64>("xDistance", 1000);
     ros::Rate loop_rate(10);  
-     
-    CvMat* threshold_matrix  = cvCreateMat(2,3,CV_32FC1);
-      
-    CvFileStorage* temp = cvOpenFileStorage("threshold_matrix.xml",NULL,CV_STORAGE_READ);
     
-    // Load the previous values of the threshold if they exist
-    if(temp)
-    {
-        threshold_matrix = (CvMat*)cvLoad("threshold_matrix.xml");
-        t1min =(int) CV_MAT_ELEM(*threshold_matrix,float,0,0) ;t2min =(int) CV_MAT_ELEM(*threshold_matrix,float,0,1) ;t3min =(int) CV_MAT_ELEM(*threshold_matrix,float,0,2);
-        t1max =(int) CV_MAT_ELEM(*threshold_matrix,float,1,0) ;t2max =(int) CV_MAT_ELEM(*threshold_matrix,float,1,1) ;t3max =(int) CV_MAT_ELEM(*threshold_matrix,float,1,2) ;
-    }
-    
-   //int camno=(**(argv+1)-'0');
-
-   //CvCapture* capture = cvCaptureFromCAM( camno);
-   CvCapture* capture =cvCreateFileCapture( argv[1]);
+    int camno=(**(argv+1)-'0');
+    CvCapture* capture = cvCaptureFromCAM( camno);
+    // CvCapture* capture =cvCreateFileCapture( argv[1]);
 
     IplImage* frame = cvQueryFrame( capture );
     
@@ -56,23 +45,23 @@ int main(int argc, char* argv[])
     //cvNamedWindow( "EdgeDetection", CV_WINDOW_AUTOSIZE );
     
     /// Create Trackbars
-     char TrackbarName1[50]="t1min";
-     char TrackbarName2[50]="t1max";
-     char TrackbarName3[50]="t2min";
-     char TrackbarName4[50]="t2max";
-     char TrackbarName5[50]="t3min";
-     char TrackbarName6[50]="t3max";
-     char TrackbarName7[50]="Rmin";
-     char TrackbarName8[50]="Rmax";
- 
-      cvCreateTrackbar( TrackbarName1, "F1", &t1min, 260 , NULL );
-      cvCreateTrackbar( TrackbarName2, "F1", &t1max, 260,  NULL  );
-      
-      cvCreateTrackbar( TrackbarName3, "F2", &t2min, 260 , NULL );
-      cvCreateTrackbar( TrackbarName4, "F2", &t2max, 260,  NULL  );
-      
-      cvCreateTrackbar( TrackbarName5, "F3", &t3min, 260 , NULL );
-      cvCreateTrackbar( TrackbarName6, "F3", &t3max, 260,  NULL  );
+    char TrackbarName1[50]="t1min";
+    char TrackbarName2[50]="t1max";
+    char TrackbarName3[50]="t2min";
+    char TrackbarName4[50]="t2max";
+    char TrackbarName5[50]="t3min";
+    char TrackbarName6[50]="t3max";
+    char TrackbarName7[50]="Rmin";
+    char TrackbarName8[50]="Rmax";
+
+    cvCreateTrackbar( TrackbarName1, "F1", &t1min, 260 , NULL );
+    cvCreateTrackbar( TrackbarName2, "F1", &t1max, 260,  NULL  );
+
+    cvCreateTrackbar( TrackbarName3, "F2", &t2min, 260 , NULL );
+    cvCreateTrackbar( TrackbarName4, "F2", &t2max, 260,  NULL  );
+
+    cvCreateTrackbar( TrackbarName5, "F3", &t3min, 260 , NULL );
+    cvCreateTrackbar( TrackbarName6, "F3", &t3max, 260,  NULL  );
 
      // cvCreateTrackbar( TrackbarName7, "Camera", &Rmin, 260 , NULL );
       //cvCreateTrackbar( TrackbarName8, "Camera", &Rmax, 260,  NULL  );
@@ -85,7 +74,7 @@ int main(int argc, char* argv[])
     height    = frame->height;
     width     = frame->width;
     step      = frame->widthStep;
-      
+
      // capture size - 
     CvSize size = cvSize(width,height);
 
@@ -97,36 +86,42 @@ int main(int argc, char* argv[])
     IplImage*  thresholded3   = cvCreateImage(size, IPL_DEPTH_8U, 1);
     IplImage*  filtered   = cvCreateImage(size, IPL_DEPTH_8U, 1);  //smoothed image
     //asking for the minimum distance where bwe fire torpedo
-    int D;
-    cin >>D;
+    int D = atoi(argv[2]);
     
     while( 1 )
     {   
         // Load threshold from the slider bars in these 2 parameters
-        hsv_min = cvScalar(t1min, t2min, t3min, 0);
-        hsv_max = cvScalar(t1max, t2max ,t3max, 0);
-   
-        std_msgs::Float32MultiArray array;
+      hsv_min = cvScalar(t1min, t2min, t3min, 0);
+      hsv_max = cvScalar(t1max, t2max ,t3max, 0);
+
+      std_msgs::Float32MultiArray array;
         // Get one frame
       
-        frame = cvQueryFrame( capture );
-     
-        cvShowImage( "RealPic", frame );
-        if( !frame )
-        {
-                fprintf( stderr, "ERROR: frame is null...\n" );
-                getchar();
-                break;
-        }
-        
+      frame = cvQueryFrame( capture );
+
+      cvShowImage( "RealPic", frame );
+      if( !frame )
+      {
+        fprintf( stderr, "ERROR: frame is null...\n" );
+        getchar();
+        break;
+      }
+
         // Covert color space to HSV as it is much easier to filter colors in the HSV color-space.
-        cvCvtColor(frame, hsv_frame, CV_BGR2HSV);
-        
+      cvCvtColor(frame, hsv_frame, CV_BGR2HSV);
+
         // Filter out colors which are out of range.
-        cvInRangeS(hsv_frame, hsv_min, hsv_max, thresholded);
+      cvInRangeS(hsv_frame, hsv_min, hsv_max, thresholded);
+
+      cvSplit( hsv_frame,thresholded1, thresholded2, thresholded3, NULL );
+      
+        // Filter out colors which are out of range.
+        cvInRangeS(thresholded1,cvScalar(t1min,0,0,0) ,cvScalar(t1max,0,0,0) ,thresholded1);
+        cvInRangeS(thresholded2,cvScalar(t2min,0,0,0) ,cvScalar(t2max,0,0,0) ,thresholded2);
+        cvInRangeS(thresholded3,cvScalar(t3min,0,0,0) ,cvScalar(t3max,0,0,0) ,thresholded3);
 
         // Memory for hough circles
-        CvMemStorage* storage = cvCreateMemStorage(0);
+      CvMemStorage* storage = cvCreateMemStorage(0);
         cvShowImage( "After Color Filtering", thresholded ); // The stream after color filtering
         // hough detector works better with some smoothing of the image
         cvSmooth( thresholded, thresholded, CV_GAUSSIAN, 9, 9 );
@@ -148,65 +143,67 @@ int main(int argc, char* argv[])
              largest_area=a;
              largest_contour_index=i;                //Store the index of largest contour
            }
-        }  
+         }  
         //Convex HULL
-        vector<vector<Point> >hull( contours.size() );
-        convexHull( Mat(contours[largest_contour_index]), hull[largest_contour_index], false );
-  
-        output_cap.write(frame);
-      
-      
-       
-        vector<Point2f>center(1);
-        vector<float>radius(1);
-        minEnclosingCircle(contours[largest_contour_index],center[0],radius[0]);    
-  
-        Mat circles=frame;
-        Point2f pt;
+         vector<vector<Point> >hull( contours.size() );
+         convexHull( Mat(contours[largest_contour_index]), hull[largest_contour_index], false );
+
+         output_cap.write(frame);
+
+         vector<Point2f>center(1);
+         vector<float>radius(1);
+         minEnclosingCircle(contours[largest_contour_index],center[0],radius[0]);    
+
+         Mat circles=frame;
+         Point2f pt;
         pt.x=320; //size of my screen
         pt.y=240;
         float distance;
         float* p;  //array to publish
-               
+
         distance=pow(radius[0]/7526.5,-.92678);  //function found using experiment
         
         circle(circles,center[0],radius[0],Scalar(0,250,0),1,8,0);  //minenclosing circle 
         circle(circles,center[0],4,Scalar(0,250,0),-1,8,0); //center is made on the screen
         circle(circles,pt,4,Scalar(150,150,150),-1,8,0);  //center of screen
         array.data.push_back(radius[0]); //publish radius
-        array.data.push_back((320-center[0].x)); 
+        array.data.push_back((320-center[0].x));
+        std_msgs::Float64 yDistance;
+        std_msgs::Float64 xDistance;
+        yDistance.data=(320-center[0].x);
         array.data.push_back((240-center[0].y));
         array.data.push_back(distance);
+        xDistance.data=distance;
         if(((320-center[0].x>-5)&&(320-center[0].x<5))&&((240-center[0].y>-5)&&(240-center[0].y)<5)) array.data.push_back(1);  //telling we are in line of center of ball
         if(distance<D) array.data.push_back(1);
         else array.data.push_back(0);
-        pub.publish(array);
-     
+        puby.publish(yDistance);
+        pubx.publish(xDistance);
+
         imshow( "circle", circles ); // Original stream with detected ball overlay
-         
+
         //cvShowImage( "HSV", hsv_frame); // Original stream in the HSV color space
-         
+
          cvShowImage( "F1", thresholded1 ); // individual filters
          cvShowImage( "F2", thresholded2 );
          cvShowImage( "F3", thresholded3 );
          
          
         //cvShowImage( "filtered", thresholded );
-        
-        cvReleaseMemStorage(&storage);
-        
-        ros::spinOnce();
+
+         cvReleaseMemStorage(&storage);
+
+         ros::spinOnce();
         // loop_rate.sleep();
 
         //If ESC key pressed, Key=0x10001B under OpenCV 0.9.7(linux version),
         //remove higher bits using AND operator
-        if( (cvWaitKey(10) & 255) == 27 ) break;
-        
-    }
-     
-     
+         if( (cvWaitKey(10) & 255) == 27 ) break;
 
-   output_cap.release(); 
-   return 0;
-  }
-  
+       }
+
+
+
+       output_cap.release(); 
+       return 0;
+     }
