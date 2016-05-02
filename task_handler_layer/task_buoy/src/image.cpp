17ea3577e69a9b3a82a7b413e7071025d108b6ea
Shikher <root@shikherverma.com>
2016-05-03 00:12:41 +0530

    fix travis CI setup

--- task_handler_layer/task_buoy/src/image.cpp
+++ task_handler_layer/task_buoy/src/image.cpp
@@ -1,5 +1,7 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
-#include <opencv2/highgui.hpp>
+#include <opencv/highgui.h>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Int8.h"
@@ -9,6 +11,7 @@
 #include <ctime>
 #include <iostream>
 #include <sstream>
+
 using namespace std;
 using namespace cv;
 
