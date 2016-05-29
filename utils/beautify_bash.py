8a79df50aa6d7b9c9ba2a1903abba0d5fce3fe5e
pkhrag <pkhrag@iitk.ac.in>
2016-05-29 22:55:05 +0530

    format py script

--- utils/beautify_bash.py
+++ utils/beautify_bash.py
@@ -68,7 +68,8 @@ class BeautifyBash:
             test_record = re.sub(r'(\A|\s)(#.*)', '', test_record, 1)
             if(not in_here_doc):
                 if(re.search('<<-?', test_record)):
-                    here_string = re.sub('.*<<-?\s*[\'|"]?([_|\w]+)[\'|"]?.*', '\\1', stripped_record, 1)
+                    here_string = re.sub(
+                        '.*<<-?\s*[\'|"]?([_|\w]+)[\'|"]?.*', '\\1', stripped_record, 1)
                     in_here_doc = (len(here_string) > 0)
             if(in_here_doc):  # pass on with no changes
                 output.append(record)
@@ -79,27 +80,33 @@ class BeautifyBash:
                 if(in_ext_quote):
                     if(re.search(ext_quote_string, test_record)):
                         # provide line after quotes
-                        test_record = re.sub('.*%s(.*)' % ext_quote_string, '\\1', test_record, 1)
+                        test_record = re.sub(
+                            '.*%s(.*)' % ext_quote_string, '\\1', test_record, 1)
                         in_ext_quote = False
                 else:  # not in ext quote
                     if(re.search(r'(\A|\s)(\'|")', test_record)):
                         # apply only after this line has been processed
                         defer_ext_quote = True
-                        ext_quote_string = re.sub('.*([\'"]).*', '\\1', test_record, 1)
+                        ext_quote_string = re.sub(
+                            '.*([\'"]).*', '\\1', test_record, 1)
                         # provide line before quote
-                        test_record = re.sub('(.*)%s.*' % ext_quote_string, '\\1', test_record, 1)
+                        test_record = re.sub(
+                            '(.*)%s.*' % ext_quote_string, '\\1', test_record, 1)
                 if(in_ext_quote):
                     # pass on unchanged
                     output.append(record)
                 else:  # not in ext quote
-                    inc = len(re.findall('(\s|\A|;)(case|then|do)(;|\Z|\s)', test_record))
+                    inc = len(re.findall(
+                        '(\s|\A|;)(case|then|do)(;|\Z|\s)', test_record))
                     inc += len(re.findall('(\{|\(|\[)', test_record))
-                    outc = len(re.findall('(\s|\A|;)(esac|fi|done|elif)(;|\)|\||\Z|\s)', test_record))
+                    outc = len(re.findall(
+                        '(\s|\A|;)(esac|fi|done|elif)(;|\)|\||\Z|\s)', test_record))
                     outc += len(re.findall('(\}|\)|\])', test_record))
                     if(re.search(r'\besac\b', test_record)):
                         if(len(case_stack) == 0):
                             sys.stderr.write(
-                                'File %s: error: "esac" before "case" in line %d.\n' % (path, line)
+                                'File %s: error: "esac" before "case" in line %d.\n' % (
+                                    path, line)
                             )
                         else:
                             outc += case_stack.pop()
@@ -113,12 +120,14 @@ class BeautifyBash:
                             outc += 1
                             case_stack[-1] -= 1
                     # an ad-hoc solution for the "else" keyword
-                    else_case = (0, -1)[re.search('^(else)', test_record) != None]
+                    else_case = (
+                        0, -1)[re.search('^(else)', test_record) != None]
                     net = inc - outc
                     tab += min(net, 0)
                     extab = tab + else_case
                     extab = max(0, extab)
-                    output.append((self.tab_str * self.tab_size * extab) + stripped_record)
+                    output.append(
+                        (self.tab_str * self.tab_size * extab) + stripped_record)
                     tab += max(net, 0)
                 if(defer_ext_quote):
                     in_ext_quote = True
@@ -128,7 +137,8 @@ class BeautifyBash:
             line += 1
         error = (tab != 0)
         if(error):
-            sys.stderr.write('File %s: error: indent/outdent mismatch: %d.\n' % (path, tab))
+            sys.stderr.write(
+                'File %s: error: indent/outdent mismatch: %d.\n' % (path, tab))
         return '\n'.join(output), error
 
     def beautify_file(self, path):
@@ -150,7 +160,8 @@ class BeautifyBash:
         error = False
         sys.argv.pop(0)
         if(len(sys.argv) < 1):
-            sys.stderr.write('usage: shell script filenames or \"-\" for stdin.\n')
+            sys.stderr.write(
+                'usage: shell script filenames or \"-\" for stdin.\n')
         else:
             for path in sys.argv:
                 error |= self.beautify_file(path)
