a3f7288afa401a915789a07dbbfd9762e4ade853
Shikher <root@shikherverma.com>
2016-06-24 02:37:13 +0530

    add gazebo to setup script

--- utils/setup.sh
+++ utils/setup.sh
@@ -15,6 +15,9 @@ sudo apt-get install -y python-catkin-pkg python-rosdep ros-$ROS_DISTRO-catkin
 sudo apt-get install python-pip
 sudo pip install autopep8
 sudo apt-get install clang-format-3.6
+# install gazebo
+curl -ssL http://get.gazebosim.org | sh
+# source ros setup script
 source /opt/ros/$ROS_DISTRO/setup.bash
 # Prepare rosdep to install dependencies.
 cd ~/catkin_ws/src
