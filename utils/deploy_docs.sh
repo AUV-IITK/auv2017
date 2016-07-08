df1eebc56cf18e0c89e8c8e1166bc34107598c08
Shikher <root@shikherverma.com>
2016-07-08 18:47:47 +0530

    fix deploy script

--- utils/deploy_docs.sh
+++ utils/deploy_docs.sh
@@ -23,30 +23,24 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into docs/html/
+# Clone the existing gh-pages for this repo into html/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO docs/html/
-cd docs/html/
+git clone $REPO html/
+cd html/
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
-# Clean docs/html/ existing contents
-rm -rf docs/html/**/* || exit 0
+# Clean html/ existing contents
+rm -rf html/ || exit 0
 
 # Run our compile script
 doCompile
 
 # Now let's go have some fun with the cloned repo
 cd catkin_ws/src/auv
-cd docs/html/
+cd html/
 git config user.name "Shikher Verma"
 git config user.email "root@shikherverma.com"
-
-# If there are no changes to the compiled docs/html/ (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the docs/html/ on this push; exiting."
-    exit 0
-fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
@@ -58,10 +52,10 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ~/catkin_ws/src/auv/deploy_key.enc -out deploy_key -d --add
 chmod 600 deploy_key
 eval `ssh-agent -s`
-ssh-add deploy_key
+echo "" | ssh-add deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
