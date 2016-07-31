c0c16ecc289f2155a88090be0c96f678788c6c46
Shikher Verma <root@shikherverma.com>
2016-07-31 09:33:18 +0000

    Deploy 9c02545ef000698bf941196de87323f58e6991ed

--- search/files_4.js
+++ search/files_4.js
@@ -1,4 +1,6 @@
 var searchData=
 [
+  ['gate_2eaction',['gate.action',['../gate_8action.html',1,'']]],
+  ['gate_5fdetection_2ecpp',['gate_detection.cpp',['../gate__detection_8cpp.html',1,'']]],
   ['generate_5fdocs_2esh',['generate_docs.sh',['../generate__docs_8sh.html',1,'']]]
 ];
--- search/files_9.js
+++ search/files_9.js
@@ -4,9 +4,10 @@ var searchData=
   ['package_2exml',['package.xml',['../debug__layer_2varun__description_2package_8xml.html',1,'']]],
   ['package_2exml',['package.xml',['../hardware__layer_2hardware__arduino_2package_8xml.html',1,'']]],
   ['package_2exml',['package.xml',['../motion__library__layer_2motion__commons_2package_8xml.html',1,'']]],
+  ['package_2exml',['package.xml',['../task__handler__layer_2task__line__detection_2package_8xml.html',1,'']]],
   ['package_2exml',['package.xml',['../task__handler__layer_2task__line__following_2package_8xml.html',1,'']]],
   ['package_2exml',['package.xml',['../motion__library__layer_2motion__forward_2package_8xml.html',1,'']]],
-  ['package_2exml',['package.xml',['../task__handler__layer_2task__line__detection_2package_8xml.html',1,'']]],
+  ['package_2exml',['package.xml',['../task__handler__layer_2task__gate_2package_8xml.html',1,'']]],
   ['package_2exml',['package.xml',['../task__handler__layer_2task__commons_2package_8xml.html',1,'']]],
   ['package_2exml',['package.xml',['../task__handler__layer_2task__buoy_2package_8xml.html',1,'']]],
   ['package_2exml',['package.xml',['../motion__library__layer_2motion__upward_2package_8xml.html',1,'']]],
@@ -17,7 +18,7 @@ var searchData=
   ['package_2exml',['package.xml',['../master__layer_2the__master_2package_8xml.html',1,'']]],
   ['package_2exml',['package.xml',['../hardware__layer_2hardware__imu_2package_8xml.html',1,'']]],
   ['pid_2ecfg',['pid.cfg',['../motion__upward_2cfg_2pid_8cfg.html',1,'']]],
-  ['pid_2ecfg',['pid.cfg',['../motion__forward_2cfg_2pid_8cfg.html',1,'']]],
   ['pid_2ecfg',['pid.cfg',['../motion__turn_2cfg_2pid_8cfg.html',1,'']]],
-  ['pid_2ecfg',['pid.cfg',['../motion__sideward_2cfg_2pid_8cfg.html',1,'']]]
+  ['pid_2ecfg',['pid.cfg',['../motion__sideward_2cfg_2pid_8cfg.html',1,'']]],
+  ['pid_2ecfg',['pid.cfg',['../motion__forward_2cfg_2pid_8cfg.html',1,'']]]
 ];
