d197afa373f9aa21a229ca65cc59db8d4ce23dfd
pkhrag <pkhrag@iitk.ac.in>
2016-08-02 23:57:19 +0530

    changed error width

--- motion_library_layer/motion_forward/src/forward.cpp
+++ motion_library_layer/motion_forward/src/forward.cpp
@@ -93,7 +93,7 @@ public:
       output = (p * error) + (i * integral) + (d * derivative);
       forwardOutputPWMMapping(output);
 
-      if (pwm.data < 2 && pwm.data > -2)
+      if (pwm.data <= 2 && pwm.data >= -2)
       {
         reached = true;
         pwm.data = 0;
--- motion_library_layer/motion_upward/src/upward.cpp
+++ motion_library_layer/motion_upward/src/upward.cpp
@@ -92,7 +92,7 @@ public:
       output = (p * error) + (i * integral) + (d * derivative);
       upwardOutputPWMMapping(output);
 
-      if (pwm.data < 2 && pwm.data > -2)
+      if (pwm.data <= 2 && pwm.data >= -2)
       {
         reached = true;
         pwm.data = 0;
