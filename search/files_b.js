63912e1d2f2d0f7549762a37649da9897be52dfc
Shikher Verma <root@shikherverma.com>
2016-08-14 00:34:11 +0000

    Deploy 2373c53c449e3c1b07feca9f5df50eca138aa406

--- search/files_b.js
+++ search/files_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['serial_5fnode_2esh',['serial_node.sh',['../serial__node_8sh.html',1,'']]],
   ['setup_2esh',['setup.sh',['../setup_8sh.html',1,'']]],
   ['sideward_2eaction',['Sideward.action',['../Sideward_8action.html',1,'']]],
   ['sideward_2ecfg',['sideward.cfg',['../sideward_8cfg.html',1,'']]],
