6c46c7065d0048b40413de75a8ca474ee0888655
harshv834 <harshv834@users.noreply.github.com>
2016-08-27 23:43:29 +0530

    Update arduino_node.cpp

--- hardware_layer/hardware_arduino/src/arduino_node.cpp
+++ hardware_layer/hardware_arduino/src/arduino_node.cpp
@@ -4,6 +4,8 @@
 #include <std_msgs/Int32.h>
 #include <std_msgs/Float64.h>
 #include <math.h>
+#include <Wire.h>
+#include "MS5837.h"
 
 #define pwmPinWest 3
 #define pwmPinEast 2
@@ -40,6 +42,8 @@ const float c117 = 441.32;
 const float s117 = -3.03;
 const float c122 = 547.39;
 const float s122 = -2.93;
+
+MS5837 sensor;
 
 int count, sum;
 bool isMovingForward = true;
@@ -307,7 +311,11 @@ ros::Publisher ps_voltage("/varun/sensors/pressure_sensor/depth", &voltage);
 void setup()
 {
   nh.initNode();
+  Wire.begin();
 
+  sensor.init();
+  
+  sensor.setFluidDensity(997); // kg/m^3 (freshwater, 1029 for seawater)
   pinMode(pwmPinEast, OUTPUT);
   pinMode(directionPinEast1, OUTPUT);
   pinMode(directionPinEast2, OUTPUT);
@@ -347,20 +355,8 @@ void setup()
 
 void loop()
 {
-  if (count == 100)
-  {
-    sum /= count;
-    voltage.data = -sum;  // negative because convention is to take upward direction as positive
-    ps_voltage.publish(&voltage);
-    sum = 0;
-    count = 0;
-  }
-  else
-  {
-    v = analogRead(analogPinPressureSensor);
-    sum += v;
-    count++;
-    delay(1);
-  }
+  sensor.read();
+  ps_voltage.publish(sensor.depth()*100);
+  delay(200);
   nh.spinOnce();
 }
