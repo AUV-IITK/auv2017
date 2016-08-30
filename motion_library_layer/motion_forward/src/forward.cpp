bca487e2883a5270e5f358d463f6cb6c4e4f295e
pkhrag <pkhrag@iitk.ac.in>
2016-08-30 20:03:08 +0530

    formated text

--- motion_library_layer/motion_forward/src/forward.cpp
+++ motion_library_layer/motion_forward/src/forward.cpp
@@ -158,7 +158,7 @@ innerActionClass *object;
 void callback(motion_forward::pidConfig &config, double level)
 {
   ROS_INFO("ForwardServer: Reconfigure Request: p= %f i= %f d=%f error band=%f", config.p, config.i, config.d,
-    config.band);
+           config.band);
   object->setPID(config.p, config.i, config.d, config.band);
 }
 
