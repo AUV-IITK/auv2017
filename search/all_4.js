f8f4dfe0a3d3214036f8992348edf554a367db5a
Shikher Verma <root@shikherverma.com>
2016-12-01 22:18:02 +0000

    Deploy dfffdd5142c783394d81cae931188b1ac94b3aa3

--- search/all_4.js
+++ search/all_4.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['taskbuoyinnerclass',['TaskBuoyInnerClass',['../classTaskBuoyInnerClass.html',1,'']]],
   ['taskgateinnerclass',['TaskGateInnerClass',['../classTaskGateInnerClass.html',1,'']]],
-  ['tasklineinnerclass',['TaskLineInnerClass',['../classTaskLineInnerClass.html',1,'']]]
+  ['tasklineinnerclass',['TaskLineInnerClass',['../classTaskLineInnerClass.html',1,'']]],
+  ['taskoctagoninnerclass',['TaskoctagonInnerClass',['../classTaskoctagonInnerClass.html',1,'']]]
 ];
