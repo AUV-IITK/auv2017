9ce7b7e54b1aeb03788e5cf73ba8a7fba1791401
Suryansh Agarwal <suryansh.a98@gmail.com>
2017-10-15 23:54:04 +0530

    added a new header file to implement the system of classes in the processing node and arduino node

---
+++ hardware_layer/hardware_arduino/src/thrustercalibraton.h
@@ -0,0 +1,34 @@
+#ifndef PWM_CONST.H
+#define PWM_CONST.H
+
+struct mode         //structure to store the three speed mode values in one direction
+{
+    int High;
+    int Med;
+    int Low;
+};
+
+using namespace std;
+class thruster{
+	public:
+		thruster();
+		~thruster();
+    
+        //tells the speed mode of the thruster
+        int mode();
+    
+        //it classifies the obtained pwm variable to given specified speed modes
+        void calibration();
+    
+        //it sets the struct values of any instance of this class
+        void setvalues();
+
+	private:
+        int pwm;        //stores the value of pwm corresponding to each thruster
+        mode clock;     //stores the value of high,med and low pwm values for clockwise direction
+        mode anticlock;    //stores the value of high,med and low pwm values for anticlockwise direction
+}
+
+#endif
+
+
