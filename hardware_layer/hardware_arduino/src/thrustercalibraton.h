ac1feffd4efc8d59bd2725eeda6c7f73ec333f1c
Suryansh Agarwal <suryansh.a98@gmail.com>
2017-10-16 00:50:53 +0530

    added a new class to implement in arduino node and also fixed some minor errors in the existing class

--- hardware_layer/hardware_arduino/src/thrustercalibraton.h
+++ hardware_layer/hardware_arduino/src/thrustercalibraton.h
@@ -1,6 +1,8 @@
 #ifndef PWM_CONST.H
 #define PWM_CONST.H
 
+using namespace std;
+
 struct mode         //structure to store the three speed mode values in one direction
 {
     int High;
@@ -8,27 +10,45 @@ struct mode         //structure to store the three speed mode values in one dire
     int Low;
 };
 
-using namespace std;
-class thruster{
+class thruster
+{
 	public:
+    
 		thruster();
-		~thruster();
     
         //tells the speed mode of the thruster
-        int mode();
+        int mode(int P);
     
         //it classifies the obtained pwm variable to given specified speed modes
-        void calibration();
+        void calibration(int PWM, int M);
     
         //it sets the struct values of any instance of this class
-        void setvalues();
+        void setvalues(int HC, int MC,int LC, int HAC, int MAC, int LAC);
 
 	private:
         int pwm_;        //stores the value of pwm corresponding to each thruster
         mode clock_;     //stores the value of high,med and low pwm values for clockwise direction
         mode anticlock_;    //stores the value of high,med and low pwm values for anticlockwise direction
-}
+};
 
+class arduino_thrust
+{
+    public:
+    
+        arduino_thrust();
+    
+        //sets the direction and pwm pins for the thrusters
+        void setPins();
+    
+        //applies analogwrite and digitalwrite functions in arduino
+        void ON(int pwm,bool Dir1,bool Dir2);
+    
+    private:
+        int An1_;   //stores the analog pin number for the thruster on arduino
+        int D1_;    //stores the direction pin number 1 for the thruster on arduino
+        int D2_;    //stores the direction pin number 2 for the thruster on arduino
+    
+};
 #endif
 
 
