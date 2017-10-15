52c9195081945be775403b91d723ef1d9ce0d24b
Suryansh Agarwal <suryansh.a98@gmail.com>
2017-10-16 00:54:24 +0530

    added a comment in the header file

--- hardware_layer/hardware_arduino/src/thrustercalibraton.h
+++ hardware_layer/hardware_arduino/src/thrustercalibraton.h
@@ -37,7 +37,7 @@ class arduino_thrust
     
         arduino_thrust();
     
-        //sets the direction and pwm pins for the thrusters
+        //sets the direction and pwm pins for the thrusters and initiate them as outputs
         void setPins(int A1, int D1, int D2);
     
         //applies analogwrite and digitalwrite functions in arduino
